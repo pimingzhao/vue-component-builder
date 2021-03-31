bc780b580d9e3ff78e1826b0a61c74d15231f375
pimzh <1105133421@qq.com>
2021-03-31 09:18:21 +0800

    feat(公共模块): 修复setting动画导致布局错误问题

--- src/views/Layout/components/Setting/Setting.vue
+++ src/views/Layout/components/Setting/Setting.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: pimzh
  * @Date: 2021-03-30 11:28:44
- * @LastEditTime: 2021-03-30 14:09:54
+ * @LastEditTime: 2021-03-31 09:17:04
  * @LastEditors: pimzh
  * @Description:
 -->
 <template>
-  <Poptip class="setting-wrapper" placement="left-start" width="400" @on-popper-show="show=true" @on-popper-hide="show=false">
+  <Poptip class="setting-wrapper" placement="left-start" width="400" :transfer="true" @on-popper-show="show=true" @on-popper-hide="show=false">
     <Icon title="设置" class="cursor-pointer" :class="{ 'animation': show }" size="25" type="md-settings" />
     <div class="api" slot="content">
       <!-- 网格线 -->
@@ -27,6 +27,21 @@ export default {
     return {
       show: false
     }
+  },
+  methods: {
+    async handleResize () {
+      if (!this.show) {
+        this.show = true
+        await this.$nextTick()
+      }
+      this.show = false
+    }
+  },
+  created () {
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.handleResize)
   }
 }
 </script>
