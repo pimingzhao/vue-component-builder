75ab701d21ee54904e09776df463664303b4744f
xiaoPi <1105133421@qq.com>
2021-04-04 09:57:27 +0800

    feat(公共模块): builder test

--- src/components/DistBuilder/DistBuilder.vue
+++ src/components/DistBuilder/DistBuilder.vue
@@ -1,16 +1,20 @@
 <!--
  * @Author: pimzh
  * @Date: 2021-03-30 11:10:06
- * @LastEditTime: 2021-04-01 11:21:40
+ * @LastEditTime: 2021-04-03 22:28:26
  * @LastEditors: pimzh
  * @Description:
 -->
 <script>
 import { mapState } from "vuex";
 import render from "../render";
+import Dragable from "../DistDragable";
 
 export default {
   name: "Builder",
+  components:{
+    Dragable
+  },
   computed: {
     ...mapState("setting", ["showGrid"])
   },
@@ -21,7 +25,13 @@ export default {
         class: "relative",
         ref: "builder"
       },
-      [this.showGrid && render.renderGrid(h, this)]
+      [this.showGrid && render.renderGrid(h, this),
+      h("Dragable", {
+        class: "w-full h-full"
+      }, [
+        "hello y"
+      ])
+      ]
     );
   },
   methods: {
