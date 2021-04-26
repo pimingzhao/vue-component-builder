b605138e16c574b2566bea417f330e299266616f
pimzh <1105133421@qq.com>
2021-04-26 14:01:01 +0800

    feat(布局组件): 样式调整；页面初始化调整

--- src/views/Layout/components/SiderMenu.vue
+++ src/views/Layout/components/SiderMenu.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: pimzh
  * @Date: 2021-03-10 17:30:04
- * @LastEditTime: 2021-04-04 10:13:54
+ * @LastEditTime: 2021-04-26 14:00:08
  * @LastEditors: pimzh
  * @Description:
 -->
 <template>
-  <ul :class="prefixCls" class="">
+  <ul :class="prefixCls">
     <li
       :class="[
         `${prefixCls}-item`,
@@ -68,6 +68,15 @@ export default {
       return this.$route.query.template || "";
     }
   },
+  created() {
+    // 添加初始数据
+    if (this.hasMenuList) {
+      this.activeList.push(this.menuList[0].name);
+      this.menuList[0].compenonts.forEach(comp => {
+        this.$store.dispatch("template/addTemplate", comp);
+      });
+    }
+  },
   methods: {
     handleSelect(name, i) {
       console.log(this.activeList.includes(name));
@@ -92,20 +101,28 @@ export default {
 </script>
 
 <style scoped>
-.sider-menu {
-  background-color: #007bbb;
-}
 .sider-menu-item {
-  background-color: #3eede7;
   padding: 14px 26px;
-  white-space: 10px;
-  color: #515a6e;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+}
+.sider-menu-span {
+  color: hsla(0, 0%, 100%, 0.7);
+}
+.sider-menu-item:hover .sider-menu-span {
+  color: #ffffff;
+}
+.sider-menu-item:not(:last-child) {
+  margin-bottom: 1px;
+}
+.menu-item .sider-menu-item-active {
   background: #363e4f;
   transition: all 0.2s ease-in-out;
 }
-.sider-menu-item-active {
-  color: #fff;
-  background: #2d8cf0 !important;
-  transition: all 0.2s ease-in-out;
+.sider-menu-item-active .sider-menu-span {
+  color: #2d8cf0;
+}
+.sider-menu-item-active:hover .sider-menu-span {
+  color: #2d8cf0;
 }
 </style>
